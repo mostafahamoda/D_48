54cdad52188bcb450643b16cc2f23e4c6dbca593
dcj835u <extern.hamoda.mostafa@cariad.technology>
2023-06-09 10:11:35 +0300

    Adding Initial files

---
+++ File.c
@@ -0,0 +1,32 @@
+
+    /*int x= 257;
+    int* ptr = &x;
+    int** ptr2 = &ptr;
+    int*** ptr3 = &ptr2;
+    int**** ptr4 = &ptr3;
+    int***** ptr5 = &ptr4;
+    //printf("%d %d %d %d %d %d",ptr,&x,x,*ptr,&ptr,sizeof(ptr));
+    printf("%d %d %d %d %d",ptr,*ptr2,**ptr3,****ptr4,*****ptr5);
+    */
+    /*int x = 513;
+    char* ptr = &x;
+    printf("%d ",ptr);
+    ptr = ptr + 1;
+    printf("%d",*ptr);*/
+int* swap(int* x,int* y){
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+int* add(int x,int y){
+
+
+}
+
+void* malloc(int size){
+
+
+}
+
+
+//int glbl; ==> flash
